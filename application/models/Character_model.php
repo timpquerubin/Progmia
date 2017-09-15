0a95f38ff2b1076b2694c7d58d8ab5877c313d16
Triston Querubin <timpquerubin@gmail.com>
2017-09-15 13:27:31 -0700

    remove functions from Game_model get_avatar_id, insert_avatar

---
+++ application/models/Character_model.php
@@ -0,0 +1,27 @@
+<?php 
+	class Character_model extends CI_Model
+	{
+		public function __construct()
+		{
+			$this->load->database();
+		}
+
+		public function gen_avatar_id($avtName)
+		{
+			$avt_rows = $this->db->get('AVATAR');
+
+			$avt_arr = $avt_rows->result_array();
+
+			$newId = md5(count($avt_arr) + $avtName);
+
+			return $newId;
+		}
+
+		public function insert_avatar($params)
+		{
+			return $this->db->insert('AVATAR', $params);
+		}
+
+		public function get_avatar_list
+	} 
+?>
--- application/models/Game_model.php
+++ application/models/Game_model.php
@@ -24,21 +24,5 @@
 
 			return $map_details->result_array();
 		}
-
-		public function gen_avatar_id($avtName)
-		{
-			$avt_rows = $this->db->get('AVATAR');
-
-			$avt_arr = $avt_rows->result_array();
-
-			$newId = md5(count($avt_arr) + $avtName);
-
-			return $newId;
-		}
-
-		public function insert_avatar($params)
-		{
-			return $this->db->insert('AVATAR', $params);
-		}
 	}
 ?>
