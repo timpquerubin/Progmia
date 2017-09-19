40962e82f849a94b47a84d1a3770f004b7bb8f56
Triston Querubin <timpquerubin@gmail.com>
2017-09-19 01:20:53 -0700

    Revert "menu_stages testing"
    This reverts commit 7dc8439b32c6bd416b4472fcb1cf8fbfb160b147.

---
+++ application/views/game/menu/menu_stages.php
@@ -0,0 +1,18 @@
+<div class="container-fluid">
+	<div class="row">
+		<ul>
+			<li><a href=""><img src=""></a></li>
+		</ul>
+	</div>
+	<div class="row">
+	<div class="container">
+			<div class="row">
+				<div class="bg-stages">
+				<img class="img-responsive stages-01" src="<?php echo base_url(); ?>assets/images/stages-01.png" style="max-height:210px !important;min-height:100px;position:absolute; top:34%; left:10%;margin:0 auto;height:auto;background-color:transparent;z-index: 5;">
+				<img class="img-responsive stages-02" src="<?php echo base_url(); ?>assets/images/stages-02.png" style="max-height:210px !important;min-height:100px;position:absolute; top:36%; left:29%;margin:0 auto;height:auto;background-color:transparent;z-index: 3;">
+				<img class="img-responsive stages-03" src="<?php echo base_url(); ?>assets/images/stages-03.png" style="max-height:215px !important;min-height:100px;position:absolute; top:56%; left:51%;margin:0 auto;height:auto;background-color:transparent;z-index: 4;">
+				<img class="img-responsive stages-04" src="<?php echo base_url(); ?>assets/images/stages-04.png" style="max-height:240px !important;min-height:100px;position:absolute; top:42%; left:60%;margin:0 auto;height:auto;background-color:transparent;">
+				</div>
+			</div>
+		</div>
+	</div> 
