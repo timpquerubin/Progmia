f20aa011fd1efd761c393c188514abd5e4fe7281
KeepMyName <jakecorbadura@gmail.com>
2017-09-19 22:13:31 +0800

    Responsive update

--- application/views/game/menu/menu_stages.php
+++ application/views/game/menu/menu_stages.php
@@ -11,11 +11,13 @@
 			</ul>
 		</div>
 	</div>
-	<div class="container">
+	<div class="container-fluid">
+		<div class="row">
 		<div class="bg-stages">
-			<img class="img-responsive stages-01" src="<?php echo base_url(); ?>assets/images/stages-01.png" style="top:34%; left:10%;margin:0 auto;height:auto;background-color:transparent;z-index: 5;">
-			<img class="img-responsive stages-02" src="<?php echo base_url(); ?>assets/images/stages-02.png" style="top:36%; left:29%;margin:0 auto;height:auto;background-color:transparent;z-index: 3;">
-			<img class="img-responsive stages-03" src="<?php echo base_url(); ?>assets/images/stages-03.png" style="top:56%; left:51%;margin:0 auto;height:auto;background-color:transparent;z-index: 4;">
-			<img class="img-responsive stages-04" src="<?php echo base_url(); ?>assets/images/stages-04.png" style="top:42%; left:60%;margin:0 auto;height:auto;background-color:transparent;">	
+			<img class="img-responsive stages-01" src="<?php echo base_url(); ?>assets/images/stages-01.png">
+			<img class="img-responsive stages-02" src="<?php echo base_url(); ?>assets/images/stages-02.png">
+			<img class="img-responsive stages-03" src="<?php echo base_url(); ?>assets/images/stages-03.png">
+			<img class="img-responsive stages-04" src="<?php echo base_url(); ?>assets/images/stages-04.png">	
+		</div>
 		</div>
 	</div>
