db4c7a39443a58d93549ef9958040190bafdb942
KeepMyName <jakecorbadura@gmail.com>
2017-09-19 21:41:38 +0800

    Responsive Testing Commit

--- application/views/game/menu/menu_stages.php
+++ application/views/game/menu/menu_stages.php
@@ -1,36 +1,21 @@
-<div class="container-fluid">
+
 	<div class="row">
 		<ul>
 			<li><a href=""><img src=""></a></li>
 		</ul>
 	</div>
-	<div class="row">
 	<div class="container">
-			<div class="row">
-<<<<<<< HEAD
-				<ul>
-					<li><a href=""><img src=""></a></li>
-				</ul>
-			</div>
-			<div class="row">
-				<div class="container" style="margin:0 !important;">
-					<div class="row">
-						<div class="bg-stages" style="margin:0 !important;">
-						<img class="img-responsive stages-01" src="<?php echo base_url(); ?>assets/images/stages-01.png" style="max-height:210px !important;min-height:100px;position:absolute; top:34%; left:10%;margin:0 auto;height:auto;background-color:transparent;z-index: 5;">
-						<img class="img-responsive stages-02" src="<?php echo base_url(); ?>assets/images/stages-02.png" style="max-height:210px !important;min-height:100px;position:absolute; top:36%; left:29%;margin:0 auto;height:auto;background-color:transparent;z-index: 3;">
-						<img class="img-responsive stages-03" src="<?php echo base_url(); ?>assets/images/stages-03.png" style="max-height:215px !important;min-height:100px;position:absolute; top:56%; left:51%;margin:0 auto;height:auto;background-color:transparent;z-index: 4;">
-						<img class="img-responsive stages-04" src="<?php echo base_url(); ?>assets/images/stages-04.png" style="max-height:240px !important;min-height:100px;position:absolute; top:42%; left:60%;margin:0 auto;height:auto;background-color:transparent;">
-						<img class="img-responsive stages-05" src="<?php echo base_url(); ?>assets/images/stages-04.png" style="max-height:240px !important;min-height:100px;position:absolute; top:42%; left:60%;margin:0 auto;height:auto;background-color:transparent;">
-						</div>
-					</div>
-=======
-				<div class="bg-stages">
-				<img class="img-responsive stages-01" src="<?php echo base_url(); ?>assets/images/stages-01.png" style="max-height:210px !important;min-height:100px;position:absolute; top:34%; left:10%;margin:0 auto;height:auto;background-color:transparent;z-index: 5;">
-				<img class="img-responsive stages-02" src="<?php echo base_url(); ?>assets/images/stages-02.png" style="max-height:210px !important;min-height:100px;position:absolute; top:36%; left:29%;margin:0 auto;height:auto;background-color:transparent;z-index: 3;">
-				<img class="img-responsive stages-03" src="<?php echo base_url(); ?>assets/images/stages-03.png" style="max-height:215px !important;min-height:100px;position:absolute; top:56%; left:51%;margin:0 auto;height:auto;background-color:transparent;z-index: 4;">
-				<img class="img-responsive stages-04" src="<?php echo base_url(); ?>assets/images/stages-04.png" style="max-height:240px !important;min-height:100px;position:absolute; top:42%; left:60%;margin:0 auto;height:auto;background-color:transparent;">
->>>>>>> 3660e8aa869aee83169161d4726a3a1f6e9ce351
-				</div>
-			</div>
+		<div class="row">
+			<ul>
+				<li><a href=""><img src=""></a></li>
+			</ul>
 		</div>
-	</div> 
+	</div>
+	<div class="container">
+		<div class="bg-stages">
+			<img class="img-responsive stages-01" src="<?php echo base_url(); ?>assets/images/stages-01.png" style="top:34%; left:10%;margin:0 auto;height:auto;background-color:transparent;z-index: 5;">
+			<img class="img-responsive stages-02" src="<?php echo base_url(); ?>assets/images/stages-02.png" style="top:36%; left:29%;margin:0 auto;height:auto;background-color:transparent;z-index: 3;">
+			<img class="img-responsive stages-03" src="<?php echo base_url(); ?>assets/images/stages-03.png" style="top:56%; left:51%;margin:0 auto;height:auto;background-color:transparent;z-index: 4;">
+			<img class="img-responsive stages-04" src="<?php echo base_url(); ?>assets/images/stages-04.png" style="top:42%; left:60%;margin:0 auto;height:auto;background-color:transparent;">	
+		</div>
+	</div>
