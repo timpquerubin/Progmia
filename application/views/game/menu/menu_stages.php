791fe0a195359c43c159053d22b05ecebd15184e
KeepMyName <jakecorbadura@gmail.com>
2017-09-23 03:03:00 +0800

    draft-stages

--- application/views/game/menu/menu_stages.php
+++ application/views/game/menu/menu_stages.php
@@ -12,28 +12,37 @@
 		</div>
 	</div>
 	-->
+	<div class="" style="background-color: #333;">
 	<div class="container-fluid">
 		<div class="bg-stage">
-			<div class="row">
-				<div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
-					<a href="#"><img class="img-responsive" src="<?php echo base_url(); ?>assets/images/stages-01.png"/></a>
-				</div>
-				<div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
-					<a href="#"><img class="img-responsive" src="<?php echo base_url(); ?>assets/images/stages-02.png"/></a>
-				</div>
+			<div class="bg-stage-2">
 			</div>
-			<div class="row">
-				<div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
-					<a href="#"><img class="img-responsive" src="<?php echo base_url(); ?>assets/images/stages-03.png"/></a>
+				<div class="wrapper">
+					<div class="row">
+						<div class="col-xs-4 col-sm-4 col-md-4 col-lg-4"><div class="stage">
+							<a href="#" style="z-index: 5;"><img class="img-responsive" src="<?php echo base_url(); ?>assets/images/stages-01.png"/></a>
+							</div>
+						</div>
+						<div class="col-xs-4 col-sm-4 col-md-4 col-lg-4"><div class="stage">
+							<a href="#"><img class="img-responsive" src="<?php echo base_url(); ?>assets/images/stages-02.png"/></a></div>
+						</div>
+						<div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+							<a href="#"><img class="img-responsive" src="<?php echo base_url(); ?>assets/images/stages-03.png"/></a>
+						</div>
+					</div>
 				</div>
-				<div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
-					<a href="#"><img class="img-responsive" src="<?php echo base_url(); ?>assets/images/stages-04.png"/></a>
+				<div class="wrapper">
+					<div class="row">
+						<div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+							<a href="#"><img class="img-responsive" src="<?php echo base_url(); ?>assets/images/stages-04.png"/></a>
+						</div>
+						<div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+							<a href="#"><img class="img-responsive" src="<?php echo base_url(); ?>assets/images/stages-05.png"/></a>
+						</div>
+					</div>
 				</div>
-			</div>
-			<div class="row">
-				<div class="col-xs-6 col-xs-offset-3 col-sm-6 col-md-6 col-lg-6">
-					<a href="#"><img class="img-responsive" src="<?php echo base_url(); ?>assets/images/stages-05.png"/></a>
-				</div>
+			<div class="bg-stage-3">
 			</div>
 		</div>
+	</div>
 	</div>
