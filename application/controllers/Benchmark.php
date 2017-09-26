73bef505577f0a589ddfd61f19aeb434b590f948
Triston Querubin <timpquerubin@gmail.com>
2017-09-26 16:40:04 +0800

    transfer rows from table map into table level

--- application/controllers/Benchmark.php
+++ application/controllers/Benchmark.php
@@ -12,17 +12,30 @@
 
 			$maps = $this->Game_model->get_all_maps();
 
-			echo "<pre>";
-			var_dump($res);
-			echo "</pre>";
+			// echo "<pre>";
+			// var_dump($res);
+			// echo "</pre>";
+			$ctr = 1;
 
-			// foreach ($maps as $m) {
+			foreach ($maps as $m) {
 				
-			// 	$map_params = array(
-			// 		'LVL_ID' =>
-			// 	);
+				$map_params = array(
+					'LVL_ID' => md5('STG_BS' + $ctr + $m['MAP_FILENAME']),
+					'LVL_GRID' => $m['MAP_GRID'],
+					'LVL_NAME' => $m['MAP_FILENAME'],
+					'LVL_SPOINT' => $m['MAP_STARTPOINT'],
+					'LVL_COLS' => $m['MAP_NUMCOLS'],
+					'LVL_MAP_HEIGHT' => $m['MAP_IMGHEIGHT'],
+					'LVL_MAP_WIDTH' => $m['MAP_IMGWIDTH'],
+					'STAGE' => 'STG_BS',
+					'LVL_NUM' => $ctr,
+				);
 
-			// }
+				$this->Game_model->add_map($map_params);
+
+				$ctr++;
+
+			}
 		}
 	}
 ?>
