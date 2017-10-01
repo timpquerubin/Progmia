3df01b545b645f31da3a7b1ccc66acc317e0f8a6
KeepMyName <jakecorbadura@gmail.com>
2017-10-02 02:32:33 +0800

    locked/unlocked stages hardcoded

--- application/views/game/menu/menu_stages.php
+++ application/views/game/menu/menu_stages.php
@@ -32,12 +32,34 @@
 				<div class="bg-stage">
 					<div class="row">
 						<ul style="list-style: none;display: inline-flex;vertical-align:middle;margin:0 auto;">
+						<?php $stage1 = true; ?>
 						<?php $i = 0; ?>
 						<?php $ctr = 1; ?>
+						<?php $exist = true ?>
 						<?php foreach ($stage_list as $stage) { ?>
+							<?php if ($exist == true){ ?>
 							<li style="position:relative;"><div class="popup unlocked-stage" onclick="myFunction<?php echo $i+1; ?>()" href="#"><span class="unlocked"><i class="fa fa-unlock" aria-hidden="true"></i></span>
 							<img class="" src="<?php echo base_url(); ?>assets/images/<?php echo $stage['STG_FILENAME']; ?>"/><span class="popuptext" id="myPopup-<?php echo $i+1; ?>"><h2><?php echo $stage['STG_NAME']; ?></h2><p><?php echo $stage['STG_DESCRIPTION']; ?></p><p>
 							<a class="level-btn btn btn-default" href="<?php echo base_url(); ?>game/levels/<?php echo $stage['STG_ID'] ?>">Enter</a>
+							<?php } else { ?>
+							<li style="position:relative;"><div class="popup locked-stage" onclick="myFunction<?php echo $i+1; ?>()" href="#"><span class="locked"><i class="fa fa-lock" aria-hidden="true"></i></span>
+							<img class="" src="<?php echo base_url(); ?>assets/images/<?php echo $stage['STG_FILENAME']; ?>"/><span class="popuptext" id="myPopup-<?php echo $i+1; ?>"><h2><?php echo $stage['STG_NAME']; ?></h2><p><?php echo $stage['STG_DESCRIPTION']; ?></p><p>
+							<a class="level-btn btn btn-default" href="<?php echo base_url(); ?>game/levels/<?php echo $stage['STG_ID'] ?>">Enter</a>
+							<?php } ?>
+							<?php $exist = false; ?>
+							<?php if ($stage1 == true){$stage1 = false;$exist = true;} ?>
+							<?php foreach ($maxlevel_list as $maxlevel) { ?>
+								<?php if ($maxlevel['STAGE'] == $stage['STG_ID']){ ?>
+								<?php if ($exist == true){break;} ?>
+								<?php foreach ($progress_list as $progress) {?>
+								<?php if ($exist == true){break;} ?>
+										<?php if($progress['LVL_ID'] == $maxlevel['LVL_ID']) {
+										$exist = true;
+										break;} ?> 
+								<?php } ?>
+								<?php } ?>
+								<?php if ($exist == true){break;} ?>
+							<?php } ?>
 							<!-- <form action="<?php echo base_url(); ?>game/levels" method="post">
 							   <input type='hidden' id='stage' name='stage' value='<?php echo $stage['STG_ID']; ?>' />
 							   <input type='hidden' id='user' name='user' value='<?php echo $h->USER_ID; ?>' />
