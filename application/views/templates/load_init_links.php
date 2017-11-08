579730de52f196041adc936ced6f467a9e3f3ae3
Triston Querubin <timpquerubin@gmail.com>
2017-11-08 19:47:38 +0800

    revert dashboard header

--- application/views/templates/load_init_links.php
+++ application/views/templates/load_init_links.php
@@ -1,7 +1,25 @@
 <script type="text/javascript" src="<?php echo base_url();?>assets/js/jquery-3.2.1.min.js""></script>
 
-<link href="<?php echo base_url(); ?>assets/css/bootstrap.min.css" rel="stylesheet">
-<link rel="stylesheet" type="text/css" href="<?php base_url(); ?>assets/css/bootstrap_cerulean.css" />
+<link rel="stylesheet" href="https://bootswatch.com/cerulean/bootstrap.min.css" />
 <script type="text/javascript" src="<?php echo base_url();?>assets/js/bootstrap.min.js""></script>
 
 <script src="https://www.promisejs.org/polyfills/promise-7.0.4.min.js"></script>
+
+<script type="text/javascript">
+		$(document).ready(function() {
+			
+			var tabActive = document.getElementById("tab-active").value;
+
+			switch(tabActive) {
+				case 'home':
+					$("#nav-pill-home").addClass("active");
+					break;
+				case 'levels':
+					$("#nav-pill-levels").addClass("active");
+					break;
+				case 'stages':
+					$("#nav-pill-stages").addClass("active");
+					break;
+			}
+		});
+</script>
