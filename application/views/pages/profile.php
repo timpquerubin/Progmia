aa16f76f0a30280116499849910915fcdb34e9a1
KeepMyName <jakecorbadura@gmail.com>
2017-11-14 20:50:07 +0800

    profile

--- application/views/pages/profile.php
+++ application/views/pages/profile.php
@@ -1,19 +1,20 @@
 
 			<div class="container-fluid">
-				<div class="col-md-3">
+				<div class="col-md-2">
 					<div class="row">
 						<div class="sidebar">
 							<ul>
-								<li><div class="avatar"><a href=""><img src="<?php echo base_url(); ?>assets/images/avatar-1.png" ></a></div></li>
+								<li><div class="avatar"><a href=""><img class="img-responsive" src="<?php echo base_url(); ?>assets/images/avatar-1.png" ></a></div></li>
 								<li><div class="info"><p>Total Points Earned:</p><p>[<?php echo $total_points; ?>]</p></div></li>
-								<li><div class="menu"><a href="<?php echo base_url(); ?>/profile/user/1/">My Progress<i class="fa fa-tasks" aria-hidden="true"></i></a></div></li>
+								<li><div class="menu"><a href="<?php echo base_url(); ?>/profile/user/1/">Progress<i class="fa fa-tasks" aria-hidden="true"></i></a></div></li>
 								<li><div class="menu"><a href="">Leaderboard<i class="fa fa-tasks" aria-hidden="true"></i></a></div></li>
+								<li><div class="img-responsivestyle=""></div></li>
 							</ul>
 						</div>
 					</div>
 				</div>
 
-				<div class="col-md-9">
+				<div class="col-md-10">
 					<div class="content">
 						<h1>Progress</h1>
 						<?php foreach ($stages_list as $stages) { ?>
