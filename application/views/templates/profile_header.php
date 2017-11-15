ef3a9a8e29c96f863ef802ef25626e87f5a60811
KeepMyName <jakecorbadura@gmail.com>
2017-11-15 18:01:39 +0800

    profile layout

--- application/views/templates/profile_header.php
+++ application/views/templates/profile_header.php
@@ -4,8 +4,9 @@
 		<link href="<?php echo base_url(); ?>assets/css/bootstrap.min.css" rel="stylesheet">
 		<link rel="stylesheet" href="https://bootswatch.com/cerulean/bootstrap.min.css" />
 		<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/css/profile.css">
-		<script type="text/javascript" src="<?php echo base_url();?>assets/js/jquery-3.2.1.min.js""></script>
-		<script type="text/javascript" src="<?php echo base_url();?>assets/js/bootstrap.min.js""></script>
+		<script type="text/javascript" src="<?php echo base_url();?>assets/js/jquery-3.2.1.min.js"></script>
+		<script type="text/javascript" src="<?php echo base_url();?>assets/js/bootstrap.min.js"></script>
+		<script type="text/javascript" src="<?php echo base_url();?>assets/js/jquery.sticky.js"></script>
 		<link rel="icon" href="<?php echo base_url(); ?>assets/images/icon_jFd_icon.ico">
 		<link href="https://fonts.googleapis.com/css?family=PT+Mono" rel="stylesheet"><link href="https://fonts.googleapis.com/css?family=PT+Sans:400,700" rel="stylesheet">
 	</head>
