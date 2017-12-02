1fc16a3609a1539ea650897e20ace7b25f7a4259
KeepMyName <jakecorbadura@gmail.com>
2017-12-02 21:26:12 +0800

    commit * simple query

--- application/models/Game_model.php
+++ application/models/Game_model.php
@@ -30,7 +30,7 @@
 		}
 		public function get_level_stage($stage = null) {
 
-			$level_stage = $this->db->query('SELECT STG_FILENAME FROM STAGE WHERE STG_ID=\''.$stage.'\';');
+			$level_stage = $this->db->query('SELECT * FROM STAGE WHERE STG_ID=\''.$stage.'\';');
 
 			return $level_stage->result_array();
 
