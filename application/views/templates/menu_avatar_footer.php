598a3ca2b3e0d1d452c63886672756fbd01522b2
KeepMyName <jakecorbadura@gmail.com>
2017-12-15 11:29:43 +0800

    push

--- application/views/templates/menu_avatar_footer.php
+++ application/views/templates/menu_avatar_footer.php
@@ -2,6 +2,7 @@
 	<script type="text/javascript" src="<?php echo base_url();?>assets/js/bootstrap.min.js""></script>
 	<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/css/slick.css"/>
 	<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/css/slick-theme.css"/>
+
 	  <script type="text/javascript">
 	    $(document).ready(function(){
 	      $('.single-item').slick();
