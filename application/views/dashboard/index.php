03e46f3c64beb3c86809928c48b5253978f70640
KeepMyName <jakecorbadura@gmail.com>
2017-12-19 11:02:40 +0800

    Dashboard Activity

--- application/views/dashboard/index.php
+++ application/views/dashboard/index.php
@@ -1,3 +1,25 @@
 <div class="container">
-	dashboard home
+	<div>
+		<h1>Players' Activity</h1>
+		<table class="table table-striped">
+			<thead>
+				<th><strong>Username</strong></th>
+				<th><strong>Stage</strong></th>
+				<th><strong>Level</strong></th>
+				<th><strong>Points Scored</strong></th>
+				<th><strong>Date Finished</strong></th>
+			</thead>
+			<tbody>
+			<!--<?php foreach ($variable as $key => $value) { ?>-->
+				<tr>
+					<td></td>
+					<td></td>
+					<td></td>
+					<td></td>
+					<td></td>
+				</tr>
+				<!--<?php }?>-->
+			</tbody>
+		</table>
+	</div>
 </div>
